64811cb0563f876be7760cedc3055e434b1acb95
Alberto Hernandez <121782036+vague2k@users.noreply.github.com>
2024-06-22 08:18:28 -0400

    Attempt to setup go releaser again (#6)
    * add: `--version` flag
    
    * add: trying to setup goreleaser again
    
    * ci(lint): adhere to lint rules
    
    * ci(release): update go releaser workflow version to 2

---
+++ cmd/version.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Release struct {
+	TagName string `json:"tag_name"`
+	Target  string `json:"target_commitish"`
+}
+
+// Gets the latest release information from the github repository
+func LatestVersion() string {
+	url := "https://api.github.com/repos/vague2k/rummage/releases/latest"
+	resp, err := http.Get(url)
+	if err != nil {
+		logger.Fatal("Error occured fetching the latest release")
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		logger.Fatal("Unexpected status code: ", resp.StatusCode)
+	}
+
+	var r Release
+	if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+		logger.Fatal("Error decoding JSON response: \n", err)
+	}
+
+	return fmt.Sprintf("%s from target branch '%s'\n", r.TagName, r.Target)
+}
