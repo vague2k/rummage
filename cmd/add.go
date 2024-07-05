3226c35a72d65fc455cec891cd8414c544738b3d
vague2k <ilovedrawing056@gmail.com>
2024-07-05 17:33:14 -0400

    refactor: cmds should take multiple args by default

--- cmd/add.go
+++ cmd/add.go
@@ -9,29 +9,14 @@ var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Add an item to the database",
 	Run: func(cmd *cobra.Command, args []string) {
-		flagMultiple, err := cmd.Flags().GetBool("multiple")
-		if err != nil {
-			logger.Fatal("Could not get '--multiple' flag value")
-		}
-
 		db, err := database.Init("")
 		if err != nil {
 			logger.Fatal(err)
 		}
 
-		if flagMultiple {
-			if _, err := db.AddMultiItems(args...); err != nil {
-				logger.Fatal(err)
-			}
-			return
-		}
-
-		if _, err := db.AddItem(args[0]); err != nil {
+		if _, err := db.AddMultiItems(args...); err != nil {
 			logger.Fatal(err)
 		}
+		return
 	},
-}
-
-func init() {
-	addCmd.Flags().BoolP("multiple", "m", false, "Add multiple items to the database")
 }
