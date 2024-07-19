7b6974c18f2f8a8009eed457d551955d77421b8d
vague2k <ilovedrawing056@gmail.com>
2024-07-19 01:30:21 -0400

    remove: unneccessary import

--- cmd/remove.go
+++ cmd/remove.go
@@ -4,14 +4,14 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/vague2k/rummage/pkg/commands"
 	"github.com/vague2k/rummage/pkg/database"
-	cmdUtils "github.com/vague2k/rummage/utils/cmd"
+	"github.com/vague2k/rummage/utils"
 )
 
 var removeCmd = &cobra.Command{
 	Use:   "remove",
 	Short: "Remove a single item or multiple items from the database. Must be a full package path",
 	Run: func(cmd *cobra.Command, args []string) {
-		flags := cmdUtils.RegisterBoolFlags(cmd, "delete-all")
+		flags := utils.RegisterBoolFlags(cmd, "delete-all")
 
 		db, err := database.Init("")
 		if err != nil {
