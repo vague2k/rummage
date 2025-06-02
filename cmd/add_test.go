4780d46f7536023bfd3cca219433b0c38827695f
vague2k <ilovedrawing056@gmail.com>
2025-06-02 09:54:42 -0400

    refactor(tests): condense add cmd tests

--- cmd/add_test.go
+++ cmd/add_test.go
@@ -8,44 +8,44 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-	t.Run("Properly adds item", func(t *testing.T) {
-		db, _ := testutils.InMemDb(t)
-		cmd := NewRootCmd(db)
-		actual := testutils.Execute(cmd, "add", "github.com/gorilla/mux")
+	tests := []struct {
+		name     string
+		input    []string
+		expected string
+	}{
+		{
+			name:     "Properly adds item",
+			input:    []string{"add", "github.com/gorilla/mux"},
+			expected: "added 1 package\n",
+		},
+		{
+			name:     "Errors properly if attempted addition does not resemble go package",
+			input:    []string{"add", "not-allowed1"},
+			expected: "the item attempted to be added to the database does not resemble a go package\n",
+		},
+		{
+			name:     "Properly adds multiple items",
+			input:    []string{"add", "github.com/gorilla/mux", "github.com/labstack/echo/v4"},
+			expected: "added 2 packages\n",
+		},
+		{
+			name:     "Errors properly if all attempted additions do not resemble go packages",
+			input:    []string{"add", "not-allowed1", "not-allowed2", "not-allowed3"},
+			expected: "stopping the adding of items early...\nissue occured when adding item not-allowed1 to the db\nadded 0 packages\n",
+		},
+		{
+			name:     "Errors properly midway through adding",
+			input:    []string{"add", "github.com/gorilla/mux", "github.com/user/echo", "not-allowed1"},
+			expected: "stopping the adding of items early...\nissue occured when adding item not-allowed1 to the db\nadded 2 packages\n",
+		},
+	}
 
-		assert.Equal(t, "added 1 package\n", actual)
-	})
-	t.Run("Errors properly if attempted addition does not resemble go package", func(t *testing.T) {
-		db, _ := testutils.InMemDb(t)
-		cmd := NewRootCmd(db)
-		actual := testutils.Execute(cmd, "add", "not-allowed1")
-
-		assert.Equal(t, "the item attempted to be added to the database does not resemble a go package\n", actual)
-	})
-}
-
-func TestMultipleAdd(t *testing.T) {
-	t.Run("Properly adds items", func(t *testing.T) {
-		db, _ := testutils.InMemDb(t)
-		cmd := NewRootCmd(db)
-		actual := testutils.Execute(cmd, "add", "github.com/gorilla/mux", "github.com/labstack/echo/v4")
-
-		assert.Equal(t, "added 2 packages\n", actual)
-	})
-
-	t.Run("Errors properly if first attempted additions do not resemble go package", func(t *testing.T) {
-		db, _ := testutils.InMemDb(t)
-		cmd := NewRootCmd(db)
-		actual := testutils.Execute(cmd, "add", "not-allowed1", "not-allowed2", "not-allowed3")
-
-		assert.Equal(t, "stopping the adding of items early...\nissue occured when adding item not-allowed1 to the db\nadded 0 packages\n", actual)
-	})
-
-	t.Run("Errors properly midway through adding", func(t *testing.T) {
-		db, _ := testutils.InMemDb(t)
-		cmd := NewRootCmd(db)
-		actual := testutils.Execute(cmd, "add", "github.com/gorilla/mux", "github.com/user/echo", "not-allowed1")
-
-		assert.Equal(t, "stopping the adding of items early...\nissue occured when adding item not-allowed1 to the db\nadded 2 packages\n", actual)
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, _ := testutils.InMemDb(t)
+			cmd := NewRootCmd(db)
+			actual := testutils.Execute(cmd, tt.input...)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
 }
